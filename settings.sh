ceb5de29a47a93c33fea53d2bd2cb1ed5ee284e5
lhleung <grandq33769@gmail.com>
2018-01-12 18:08:33 +0800

    Fix syntax

--- settings.sh
+++ settings.sh
@@ -2,14 +2,19 @@
 (echo $(uname -a)| grep -Eq  .*Linux.*) && os="Linux"
 echo "Operating System: $os"
 
-[ "$os" == "Mac" ]] && tool="brew" 
+[ "$os" == "Mac" ] && tool="brew" 
 [ "$os" == "Linux" ] && tool="apt-get" 
 echo $tool
 
-sudo $tool install vim -y
-sudo $tool install tmux -y
-sudo $tool install openssh-clinets -y
-sudo $tool install zsh -y
+$tool install vim -y
+$tool install tmux -y
+$tool install openssh-clinets -y
+$tool install python -y
+$tool install python3 -y
+$tool install python-pip -y
+$tool install python-pip3 -y
+$tool install zsh -y
+
 
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
