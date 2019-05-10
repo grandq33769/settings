8fc1da922f33b4c003ee57f87a7659351baebfa6
lhleung <grandq33769@gmail.com>
2019-05-10 13:11:22 +0800

    feat(func.sh): Add stop & delete first-n docker func
    N

--- src/.llh/func.sh
+++ src/.llh/func.sh
@@ -1,1 +1,15 @@
 fr() { find . -name "*$1*" | grep --color=auto $1}
+dsfn() {
+  # Stop first-n docker 
+  # $1(n):int: int for first-n
+  for i in {1..$1} 
+  do
+    docker stop "$(dpa|awk '{print $1}'|head -$(($i+1))|tail -1)"
+  done
+}
+ddfn() {
+  for i in {1..$1}
+  do
+   docker rm "$(dpa|awk '{print $1}'|head -2|tail -1)"
+  done
+}
